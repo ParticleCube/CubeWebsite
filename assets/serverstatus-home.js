6b70d44a0f65960661982b8bca6c30ad6d90a0af
LittleChest <81231195+LittleChest@users.noreply.github.com>
2023-09-16 23:52:29 +0800

    总体观感

--- assets/serverstatus-home.js
+++ assets/serverstatus-home.js
@@ -10,7 +10,7 @@
         document.getElementById("serverInd").classList.add("text-green-600");
         document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">现在游玩：${data.players.online} 在线</span>`;
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;服务器在线！&ensp;&middot;&ensp;${data.players.online} 位玩家正在游玩中</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;服务器在线&ensp;&middot;&ensp;${data.players.online} 位玩家正在游玩中</p>`;
         document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;在线，${data.players.online} 位玩家正在游玩中</p>`;
         {% endif %}
     }
